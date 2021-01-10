e94863002c3d5022ee38ef797c23f32b897e3014
yslc8226 <ysabeacatinan@gmail.com>
2021-01-10 10:31:35 +0800

    Angry_Birds

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var bird, slingShot;
+var bird, sling;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -37,9 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+   
+    sling = new SlingShot(bird.body,{x:200,y:100});
 }
 
 function draw(){
@@ -63,6 +62,13 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    sling.display();    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+    sling.fly();
 }
